1322ac3503bb5eedbcf08e81da35e3d169f4d3be
Pradeep <pawarpradeep1707@gmail.com>
2025-01-31 21:19:55 +0530

    Start of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_database/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      de
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
     );
   }
 }
